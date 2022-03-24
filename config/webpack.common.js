d1b1e0b640b0c13563910334ad7af565a0d231fa
mikezano <mikezano@gmail.com>
2022-03-23 22:08:32 -0700

    first commit

---
+++ config/webpack.common.js
@@ -0,0 +1,34 @@
+const { VueLoaderPlugin } = require('vue-loader')
+
+module.exports = {
+    resolve: {
+        extensions: ['.js', '.vue']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: ['@babel/plugin-transform-runtime'],
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                  'vue-style-loader',
+                  'css-loader'
+                ]
+              }            
+        ]
+    },
+    plugins: [new VueLoaderPlugin()]
+}
---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{ message }}
+  </div>
+  <Button />
+</template>
+
+<script>
+import Button from './Button.vue'
+export default {
+  data() {
+    return { message: "Hello Vue!" };
+  },
+  components:{
+    Button
+  }
+};
+</script>
